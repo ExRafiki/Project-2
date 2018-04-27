e67e208d8511f8c92756343b53fe0a5cfd832e90
ExRafiki <shiizzlle00@gmail.com>
2018-04-27 09:58:19 +0100

    pre heroku deploy

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const databaseURI = 'mongodb://localhost/justtv';
+const databaseURI = process.env.MONGODB_URI || 'mongodb://localhost/justtv';
 const port        = process.env.PORT || 4000;
 //------------------------------------------------------------------------------
 module.exports = {databaseURI, port};
